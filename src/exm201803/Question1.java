be954c339c3d5272ec56f6a54378ff5596d1cc07
ozel <ozal220@163.com>
2018-08-25 17:36:40 +0800

    20180301

--- src/exm201803/Question1.java
+++ src/exm201803/Question1.java
@@ -1,7 +1,25 @@
 package exm201803;
 
+import java.util.Scanner;
+
 public class Question1 {
     public static void main(String[] args) {
-        System.out.println("begin!");
+        int sum = 0;
+        int nice = 0;
+        Scanner cin = new Scanner(System.in);
+        while (cin.hasNextInt()) {
+            int n = cin.nextInt();
+            if (n == 0)
+                break;
+            else if (n == 1){
+                nice = 0;
+                sum += 1;
+            }
+            else if (n == 2){
+                nice += 2;
+                sum += nice;
+            }
+        }
+        System.out.println(sum);
     }
 }
