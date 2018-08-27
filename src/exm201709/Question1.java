a8e38c6c700d07a0e36c2e14de15928e5af42d78
ozel <ozal220@163.com>
2018-08-27 22:07:58 +0800

    20170901-打酱油

---
+++ src/exm201709/Question1.java
@@ -0,0 +1,25 @@
+package exm201709;
+
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        int result = 0;
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt() / 10;
+        if (n < 3) {
+            result = n;
+        } else if (n >= 3 && n < 5) {
+            result = n + 1;
+        } else  {
+            int a = (n / 5);
+            n %= 5;
+            result = a * 7;
+            if (n >= 3)
+                result += n + 1;
+            else
+                result += n;
+        }
+        System.out.println(result);
+    }
+}
