e6e2f80c955f93fd4f8ceaff4a00ae38ca5e1306
ozel <ozal220@163.com>
2018-08-27 22:09:29 +0800

    20170901-打酱油

--- src/exm201709/Question1.java
+++ src/exm201709/Question1.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Question1 {
     public static void main(String[] args) {
-        int result = 0;
+        int result;
         Scanner cin = new Scanner(System.in);
         int n = cin.nextInt() / 10;
         if (n < 3) {
             result = n;
-        } else if (n >= 3 && n < 5) {
+        } else if (n < 5) {
             result = n + 1;
         } else  {
             int a = (n / 5);
